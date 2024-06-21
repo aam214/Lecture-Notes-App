41e1f7f0145198247d1a8530d9da335480f53d4b
Ariana <152944814+aam214@users.noreply.github.com>
2024-06-21 14:18:32 -0500

    Added <link rel="shortcut icon" href="#"> to HTML to remove console error. Edited import NotesAPI in app.js. Methods of sort and push added to js files. GetItem and setItem from local storage. toISO string and ID added to saveNote function.

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -1,10 +1,18 @@
-export default class NotesAPI{
-  static grabNotes(){
-
+export default class NotesAPI {
+  
+  static grabNotes() {
+  const notes = JSON.parse(localStorage.getItem("items") || "[]");
+  return notes;
+  
   }
 
 static saveNote(save){
+const notes = NotesAPI.getAllNotes();
+notes.push(save);
 
+save.id = Math.floor(Math.random() * 1000);
+save.updated = new Date().toISOString();
+localStorage.setItem( items, JSON.stringify(notes));
 }
 
 
