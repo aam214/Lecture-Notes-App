2fa0298382f966ebf9137111a1208246d372e2dc
Ariana <152944814+aam214@users.noreply.github.com>
2024-06-23 18:13:56 -0500

    Removed dummy data from innerHTML so JS localStorage can be added. Reworded textarea.

--- js/NotesUI.js
+++ js/NotesUI.js
@@ -7,24 +7,15 @@ export default class NotesUI{
     this.onDelete = onDelete;
     this.root.innerHTML = `
     <div class="side-bar">
-  <button class="button-53" role="button">Save Notes</button>
-  <div class="note-list">
-    <div class="select-notes">
-  <div class="list-heading">Cell Biology</div>
-  <div class="list-body">mDNA is acquired from maternal DNA.</div>
-  <div class="list-time-stamp">Tuesday 06/19/24 10:02 AM</div>
-  </div>
-  </div>
-</div>
-  </div>
-  <div class="preview">
-  <input class="title-input" type="text" placeholder="Title for Notes...">
-  <textarea class="notes-body">Notes...</textarea>
-</div>
-    
-    
-    
-    `
+    <button class="button-53" role="button">Save Notes</button>
+    <div class="note-list">
+ 
+    </div>
+    <div class="preview">
+    <input class="title-input" type="text" placeholder="Title for Notes...">
+    <textarea class="notes-body">Enter Notes...</textarea>
+    </div>
+    `;
 
 
   }
